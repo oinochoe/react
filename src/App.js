7ea07029a6cdf13e103f37db3af105cb84175bc5
Yeognmin Kim <copstyle86@gmail.com>
2019-03-25 07:41:31 +0900

    react 공부

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const text = '이랑';
+    const condition = true;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <h1>안녕!</h1>
+        <h2>{text}</h2>
+        {
+          condition ? "참" : "거짓"
+        }
+      </Fragment>
     );
   }
 }
