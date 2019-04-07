d20bb588ac94d4219b3f34f99e70dac0bff0c9e7
Yeognmin Kim <copstyle86@gmail.com>
2019-04-08 00:15:46 +0900

    핵심은 [e.target.name] : e.target.value

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -3,24 +3,20 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
 
     state = {
+        username:'',
         message: ''
     }
 
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleChange(e) {
+    handleChange = (e) => {
         this.setState({
-            message: e.target.value
+            [e.target.name] : e.target.value
         });
     }
 
-    handleClick() {
-        alert(this.state.message)
+    handleClick = () => {
+        alert(this.state.username + ':' + this.state.message);
         this.setState({
+            username:'',
             message: ''
         });
     }
@@ -29,6 +25,13 @@ class EventPractice extends Component {
         return (
             <div>
                 <h1>이벤트 연습</h1>
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="유저명을 입력해보세요"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
                 <input
                     type="text"
                     name="message"
