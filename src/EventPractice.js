358f38fceb1e2aa4353ccf4777975dd0598198f6
Yeognmin Kim <copstyle86@gmail.com>
2019-04-07 23:20:11 +0900

    constructor 사용해서 this와 각각의 메서드를 binding

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -6,6 +6,25 @@ class EventPractice extends Component {
         message: ''
     }
 
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleClick() {
+        alert(this.state.message)
+        this.setState({
+            message: ''
+        });
+    }
+
     render() {
         return (
             <div>
@@ -15,14 +34,9 @@ class EventPractice extends Component {
                     name="message"
                     placeholder="아무거나 입력해보세요"
                     value={this.state.message}
-                    onChange={
-                        (e) => {
-                            this.setState({
-                                message: e.target.value
-                            })
-                        }
-                    }
+                    onChange={this.handleChange}
                 />
+                <button onClick={this.handleClick}>확인</button>
             </div>
         );
     }
