754ddd59f9173424a5dd7f16b73fa3532d90fe60
Yeognmin Kim <copstyle86@gmail.com>
2019-04-13 16:35:58 +0900

    slice와 전개연산자를 이용하여 index번째 값을 제외 나머지 값들을 다시 배열에 넣어줌

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -11,9 +11,9 @@ class IterationSample extends Component {
         this.setState({
             name: e.target.value
         }, () => {
-           /*  setTimeout(() => {
-                this.button.focus();
-            }, 1000); */
+            /*  setTimeout(() => {
+             *  this.button.focus();
+             }, 1000); */
         });
     }
 
@@ -26,9 +26,30 @@ class IterationSample extends Component {
         this.input.focus();
     }
 
+    handleRemove = (index) => {
+        // 편의상 name의 레퍼런스를 미리 만듭니다.
+        const { names } = this.state;
+
+        /* 배열을 자르는 내장 함수 slice와
+         * 전개 연산자(...)를 사용하여 index번째 값을 제외한 값들을 배열에 넣어 줍니다.
+         */
+
+        this.setState({
+            names: [
+                ...names.slice(0, index),
+                ...names.slice(index + 1, names.length)
+            ]
+        })
+    }
+
     render() {
         const nameList = this.state.names.map(
-            (name, index) => (<li key={index}>{name}</li>)
+            (name, index) =>
+                (<li
+                    key={index}
+                    onDoubleClick={() => this.handleRemove(index)}>
+                    {name}
+                </li>)
         );
 
         return (
