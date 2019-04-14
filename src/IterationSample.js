558556171c9ee6d3b75dbccbb63918145955d462
Yeognmin Kim <copstyle86@gmail.com>
2019-04-14 19:11:01 +0900

    필터로 배열 제외를 좀 더 쉽게 만들기

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -29,17 +29,10 @@ class IterationSample extends Component {
     handleRemove = (index) => {
         // 편의상 name의 레퍼런스를 미리 만듭니다.
         const { names } = this.state;
-
-        /* 배열을 자르는 내장 함수 slice와
-         * 전개 연산자(...)를 사용하여 index번째 값을 제외한 값들을 배열에 넣어 줍니다.
-         */
-
         this.setState({
-            names: [
-                ...names.slice(0, index),
-                ...names.slice(index + 1, names.length)
-            ]
-        })
+            // filter로 index번째를 제외한 원소만 있는 새로운 배열 생성
+            names: names.filter((item, i) => i !== index)
+        });
     }
 
     render() {
