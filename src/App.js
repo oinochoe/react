7b7ce0003cbe96c2ca25cace554bfd7e5a069767
Yeognmin Kim <copstyle86@gmail.com>
2019-04-21 22:07:41 +0900

    LifeCycleSample로 리액트 순환주기 테스트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+// 랜덤 색상을 생성
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#00000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
-      <IterationSample />
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
     );
   }
 }
