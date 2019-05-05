faea37686d30adb35f868b8befdd6d662cf4b312
Yeongmin Kim <copstyle86@gmail.com>
2019-05-05 16:13:06 +0900

    css In Js의 한 방법인 Styled-components로  버튼 만들기

---
+++ src/components/StyledButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div `
+    border: 1px solid black;
+    display:inline-block;
+    padding:1rem;
+    border-radius:3px;
+    font-size: ${(props) => props.fontSize};
+    &:hover{
+        background:black;
+        color:white;
+    }
+`;
+
+const StyledButton = ({children, ...rest}) => {
+    return (
+        <Wrapper fontSize="1.25rem" {...rest}>
+            {children}
+        </Wrapper>
+    );
+};
+
+export default StyledButton;
