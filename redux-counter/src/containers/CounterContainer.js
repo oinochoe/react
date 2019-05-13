7dd9ecc7b4130acb61ec9fe1b667ae9ddc3af226
oinochoe <oinochoe@naver.com>
2019-05-13 09:10:36 +0900

    [redux-rducer] 간단한 서브 리듀서 사용 방법

--- redux-counter/src/containers/CounterContainer.js
+++ redux-counter/src/containers/CounterContainer.js
@@ -29,8 +29,8 @@ export function getRandomColor() {
 
 // store 안의 state 값을 props로 연결합니다.
 const mapStateToProps = (state) => ({
-    color: state.color,
-    number: state.number
+    color: state.colorData.color,
+    number: state.numberData.number
 });
 
 /* 액션 생성 함수를 사용하여 액션을 생성하고,
--- redux-counter/src/reducers/index.js
+++ redux-counter/src/reducers/index.js
@@ -1,38 +1,25 @@
-import * as types from '../actions/ActionTypes';
+import number from './number';
+import color from './color';
 
-// 초기상태 정의
-const initialState = {
-    color: 'black',
-    number: 0
-};
+import { combineReducers } from 'redux';
 
-/*  리듀서 함수를 정의합니다. 리듀서는 state와 action을 파라미터로 받습니다.
-    state가 undefeind일 때(스토어가 생성될 때) state 기본 값을 initialState로 사용합니다.
-    action.type에 따라 다른 작업을 하고, 새 상태를 만들어서 반환합니다.
-    이 때 주의할 점은 state를 직접 수정하면 안되고,
-    기존 상태 값에 원하는 값을 덮어쓴 새로운 객체를 만들어서 반환해야만 합니다.
+/*  서브 리듀서들을 하나로 합칩니다.
+    combineReducer를 실행하고 나면, 나중에 store 형태를 파라미터로 전달한 객체모양대로 만듭니다.
+    지금은 다음과 같이 만듭니다.
+    {
+        numberData {
+            number: 0
+        },
+        colorData {
+            color: 'black'
+        }
+
+    }
 */
 
-function counter(state = initialState, action) {
-    switch (action.type) {
-        case types.INCREMENT:
-            return {
-                ...state,
-                number: state.number + 1
-            };
-        case types.DECREMENT:
-            return {
-                ...state,
-                number: state.number - 1
-            };
-        case types.SET_COLOR:
-            return {
-                ...state,
-                color: action.color
-            };
-        default:
-            return state;
-    }
-};
+const reducers = combineReducers({
+    numberData: number,
+    colorData: color
+});
 
-export default counter;
+export default reducers;
