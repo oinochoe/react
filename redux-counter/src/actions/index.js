20812dba07c9b6ca7734d42b9bc98da545181fdc
oinochoe <oinochoe@naver.com>
2019-05-15 09:58:35 +0900

    [redux-counter] 멀티 카운터를 위한 액션 수정

--- redux-counter/src/actions/index.js
+++ redux-counter/src/actions/index.js
@@ -5,16 +5,30 @@ action 객체를 만드는 액션 생성 함수들을 선언합니다(action cre
 
 import * as types from './ActionTypes';
 
-export const increment = () => ({
-    type: types.INCREMENT
+// 카운터를 새로 만들 때 기본 색상을 받을 수 있도록 color가 파라미터로 설정..
+export const create = (color) => ({
+    type: types.CREATE,
+    color
 });
 
-export const decrement = () => ({
-    type: types.DECREMENT
+// 맨 마지막 카운터를 삭제하기 때문에 따로 index값 없음
+export const remove = () => ({
+    type: types.REMOVE
+});
+
+export const increment = (index) => ({
+    type: types.INCREMENT,
+    index
+});
+
+export const decrement = (index) => ({
+    type: types.DECREMENT,
+    index
 });
 
 // 다른 액션 생성자들과 달리 파라미터를 갖고 있습니다.
-export const setColor = (color) => ({
+export const setColor = (index, color) => ({
     type: types.SET_COLOR,
+    index,
     color
 });
