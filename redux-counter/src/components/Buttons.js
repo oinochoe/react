a4479b229451a687ef4160a65fc922a3f6b8e94d
oinochoe <oinochoe@naver.com>
2019-05-16 09:54:00 +0900

    [React-redux] 프리젠테이셔널 컴포넌트(buttons.js)작성중 (현재 작동x)

---
+++ redux-counter/src/components/Buttons.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Buttons.css';
+
+const Buttons = ({onCreate, onRemove}) => {
+    // 진행중 현재 작동 안함 상태
+}
--- redux-counter/src/reducers/index.js
+++ redux-counter/src/reducers/index.js
@@ -1,25 +1,71 @@
-import number from './number';
-import color from './color';
+import * as types from '../actions/ActionTypes';
 
-import { combineReducers } from 'redux';
-
-/*  서브 리듀서들을 하나로 합칩니다.
-    combineReducer를 실행하고 나면, 나중에 store 형태를 파라미터로 전달한 객체모양대로 만듭니다.
-    지금은 다음과 같이 만듭니다.
-    {
-        numberData {
+// 초기 상태 정의
+const initialState = {
+    counters: [
+        {
+            color: 'black',
             number: 0
-        },
-        colorData {
-            color: 'black'
         }
+    ]
+};
 
+// 리듀서 함수에서 카운터 추가 및 삭제 구현
+function counter(state = initialState, action) {
+    // 레퍼런스 생성
+    const { counters } = state;
+
+    switch (action.type) {
+        case types.CREATE:
+            return {
+                counter: [
+                    ...counters,
+                    {
+                        color: action.color,
+                        number: 0
+                    }
+                ]
+            };
+        case types.REMOVE:
+            return {
+                counters: counters.slice(0, counters.length - 1)
+            };
+        case types.INCREMENT:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index), // 선택한 인덱스의 전 아이템들
+                    {
+                        ...counters[action.index], // 기존 객체에
+                        number: counters[action.index].number + 1 // 새 number 값 덮어 쓰기
+                    },
+                    ...counters.slice(action.index + 1, counters.length) // 선택한 인덱스의 다음 아이템들
+                ]
+            };
+        case types.DECREMENT:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number -1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            }
+        case types.SET_COLOR:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        color: action.color
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            }
+        default:
+            return state;
     }
-*/
+}
 
-const reducers = combineReducers({
-    numberData: number,
-    colorData: color
-});
-
-export default reducers;
+export default counter;
