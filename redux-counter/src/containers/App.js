7424935aa75d542224b8eec393e81e4b27e4e192
Yeongmin Kim <oinochoe@naver.com>
2019-05-17 22:52:34 +0900

    [React-redux] App 컴포넌트 수정하여 기능 작동 완료

--- redux-counter/src/containers/App.js
+++ redux-counter/src/containers/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-//import Counter from '../components/Counter';
-import CounterContainer from './CounterContainer';
+import Buttons from '../components/Buttons';
+import CounterListContainer from './CounterListContainer';
+import getRandomColor from '../lib/getRandomColor';
 
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class App extends Component {
 	render() {
+		const { onCreate, onRemove } = this.props;
 		return (
-			<div>
-                <CounterContainer/>
+			<div className="App">
+				<Buttons
+					onCreate={onCreate}
+					onRemove={onRemove}
+				/>
+				<CounterListContainer/>
 			</div>
-		)
+		);
 	}
 }
 
-export default App;
+// 액션 생성 함수 준비
+const mapToDispatch = (dispatch) => ({
+	onCreate: () => dispatch(actions.create(getRandomColor())),
+	onRemove: () => dispatch(actions.remove())
+});
+
+// 리덕스에 연결시키고 내보냅니다.
+export default connect(null, mapToDispatch)(App);
