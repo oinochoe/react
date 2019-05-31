5f0229991308dcfc732c673a611551f0ba7a6ad6
Yeongmin Kim <oinochoe@naver.com>
2019-05-31 18:11:25 +0900

    [redux-starter] redux 로거 사용과 redux-thunk의 설치

--- redux-starter-kit/src/store.js
+++ redux-starter-kit/src/store.js
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
 
-// 미들웨어가 여러 개일 때는 파라미터로 전달하면 됩니다. 예 : applyMiddleware(a,b,c)
-// 미들웨어 순서는 여기에서 전달한 파라미터 순서대로 지정합니다.
-const store = createStore(modules, applyMiddleware(loggerMiddleware))
+import { createLogger } from 'redux-logger';
+
+/*  로그 미들웨어를 만들 때 커스터마이징 할 수 있습니다.
+    https://github.com/evgenyrodionov/redux-logger#options
+ */
+
+const logger = createLogger();
+
+const store = createStore(modules, applyMiddleware(logger))
 
 export default store;
