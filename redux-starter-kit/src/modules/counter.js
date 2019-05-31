13ab374adc2e75fabb039d586618bf63e83026db
Yeongmin Kim <oinochoe@naver.com>
2019-05-31 18:17:59 +0900

    [redux-starter] redux-thunk 적용 후 카운터

--- redux-starter-kit/src/modules/counter.js
+++ redux-starter-kit/src/modules/counter.js
@@ -1,3 +1,4 @@
+// src/module/counter.js
 import { handleActions, createAction } from 'redux-actions';
 
 const INCREMENT = 'INCREMENT';
@@ -5,6 +6,20 @@ const DECREMENT = 'DECREMENT';
 
 export const increment = createAction(INCREMENT);
 export const decrement = createAction(DECREMENT);
+
+export const incrementAsync = () => dispatch => {
+    // 1초 뒤 액션 디스패치
+    setTimeout(() => {
+        dispatch(increment())
+    }, 1000);
+}
+
+export const decrementAsync = () => dispatch => {
+    // 1초 뒤 액션 디스패치
+    setTimeout(() => {
+        dispatch(decrement())
+    }, 1000);
+}
 
 export default handleActions({
     [INCREMENT]: (state, action) => state + 1,
--- redux-starter-kit/src/store.js
+++ redux-starter-kit/src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 /*  로그 미들웨어를 만들 때 커스터마이징 할 수 있습니다.
     https://github.com/evgenyrodionov/redux-logger#options
@@ -9,6 +10,6 @@ import { createLogger } from 'redux-logger';
 
 const logger = createLogger();
 
-const store = createStore(modules, applyMiddleware(logger))
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk))
 
 export default store;
