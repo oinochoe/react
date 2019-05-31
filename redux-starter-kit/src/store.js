5f35328390b3788e82aac2512728a8e427ae1f0f
Yeongmin Kim <oinochoe@naver.com>
2019-05-31 17:59:33 +0900

    [redux-starter] middleware 직접 만들기 셋팅

--- redux-starter-kit/src/store.js
+++ redux-starter-kit/src/store.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(modules)
+// 미들웨어가 여러 개일 때는 파라미터로 전달하면 됩니다. 예 : applyMiddleware(a,b,c)
+// 미들웨어 순서는 여기에서 전달한 파라미터 순서대로 지정합니다.
+const store = createStore(modules, applyMiddleware(loggerMiddleware))
 
 export default store;
