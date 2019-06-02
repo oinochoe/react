29cc792d65776051f4f5471842b3609452fccca5
Yeongmin Kim <oinochoe@naver.com>
2019-06-02 20:40:44 +0900

    [redux-starter] redux-promise-middleware 사용

--- redux-starter-kit/src/store.js
+++ redux-starter-kit/src/store.js
@@ -2,14 +2,19 @@ import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 
 import { createLogger } from 'redux-logger';
-import ReduxThunk from 'redux-thunk';
+/* import ReduxThunk from 'redux-thunk'; */
+
+import { createPromise } from 'redux-promise-middleware';
 
 /*  로그 미들웨어를 만들 때 커스터마이징 할 수 있습니다.
     https://github.com/evgenyrodionov/redux-logger#options
  */
 
 const logger = createLogger();
+const pm = createPromise({ 
+    promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE']
+});
 
-const store = createStore(modules, applyMiddleware(logger, ReduxThunk))
+const store = createStore(modules, applyMiddleware(logger, pm))
 
 export default store;
