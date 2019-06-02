21c3e3f6b3210039bb44c0d85659fde7b52c7f9a
Yeongmin Kim <oinochoe@naver.com>
2019-06-03 01:28:30 +0900

    [redux-starter] redux-pender 사용 완료

--- redux-starter-kit/src/modules/post.js
+++ redux-starter-kit/src/modules/post.js
@@ -1,5 +1,5 @@
-import { handleActions } from 'redux-actions';
-
+import { handleActions, createAction } from 'redux-actions';
+import { pender, applyPenders } from 'redux-pender';
 import axios from 'axios';
 
 function getPostAPI(postId) {
@@ -7,49 +7,53 @@ function getPostAPI(postId) {
 }
 
 const GET_POST = 'GET_POST';
-const GET_POST_PENDING = 'GET_POST_PENDING';
-const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'GET_POST_FAILURE';
 
-export const getPost = (postId) => ({
-	type: GET_POST,
-	payload: getPostAPI(postId)
-});
+/* redux-pender의 액션 구조는 Flux standard action(https://github.com/acdlite/flux-standard-action)을
+   따르기 때문에, createAction으로 액션을 만들 수 있습니다.
+   두 번째로 들어가는 파라미터는 Promise를 반환하는 함수여야 합니다.
+*/
+export const getPost = createAction(GET_POST, getPostAPI);
 
 const initialState = {
-	pending: false,
-	error: false,
+	// 요청이 진행 중인지, 오류가 발생했는지 여부는 더 이상 직접 관리할 필요가 없습니다.
+	// penderReducer가 담당합니다.
 	data: {
-		title: '',
-		body: ''
+		title:'',
+		body:''
 	}
 }
 
-export default handleActions({
-	[GET_POST_PENDING]: (state, action) => {
-		return {
-			...state,
-			pending: true,
-			error: false
-		}
-	},
-	[GET_POST_SUCCESS]: (state, action) => {
-		const { title, body } = action.payload.data;
+const reducer = handleActions({
+	// 다른 일반 액션들을 관리...
+	...pender({
+		// pender로도 관리 가능하지만 applyPenders로 많은 penders를 관리할 수 있다.
+	})
+	
+}, initialState);
 
-		return {
-			...state,
-			pending: false,
-			data: {
-				title,
-				body
+export default applyPenders(reducer, [
+	{	
+		type: GET_POST, // type이 주어지면 이 type에 접미사를 붙인 액션 핸들러들이 담긴 객체를 만듭니다.
+		/* 요청 중일 때와 실패했을 때 추가로 해야할 작업이 있다면
+		   이렇게 onPending과 onFailure를 추가하면 됩니다.
+		   onPending: (state, action) => state,
+		   onFailure: (state, action) => state
+		*/
+		onSuccess: (state, action) => {
+			// 성공 했을 때 해야할 작업이 따로 없으면 이 함수 또한 생략이 가능합니다.
+			const { title, body } = action.payload.data;
+			return {
+				data: {
+					title,
+					body
+				}
 			}
 		}
+		// 함수를 생략했을 때 기본 값으로는 (state, action) => state를 설정합니다.
+		// (state를 그대로 반환한다는 말임..)
 	},
-	[GET_POST_FAILURE]: (state, action) => {
-		return {
-			...state,
-			pending: false,
-			erorr: true
-		}
-	}
-}, initialState);
+	/* 다른 pender 액션들
+	 { type: GET_SOMETHING, onSuccess: (state, action) => ...},
+	 { type: GET_SOMETHING, onSuccess: (state, action) => ...}
+	*/
+]);
--- redux-starter-kit/src/store.js
+++ redux-starter-kit/src/store.js
@@ -4,17 +4,14 @@ import modules from './modules';
 import { createLogger } from 'redux-logger';
 /* import ReduxThunk from 'redux-thunk'; */
 
-import { createPromise } from 'redux-promise-middleware';
+import penderMiddleware from 'redux-pender';
 
 /*  로그 미들웨어를 만들 때 커스터마이징 할 수 있습니다.
     https://github.com/evgenyrodionov/redux-logger#options
  */
 
 const logger = createLogger();
-const pm = createPromise({ 
-    promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE']
-});
 
-const store = createStore(modules, applyMiddleware(logger, pm))
+const store = createStore(modules, applyMiddleware(logger, penderMiddleware()))
 
 export default store;
