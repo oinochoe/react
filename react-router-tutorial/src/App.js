5609c8cfcb49a3529ab29aee1d06c5f3872f68e1
Yeongmin Kim <oinochoe@naver.com>
2019-06-06 19:18:28 +0900

    [react-router] query-string 사용하여 query 값으로 돔 변경 및 메뉴 Link 사용

--- react-router-tutorial/src/App.js
+++ react-router-tutorial/src/App.js
@@ -5,10 +5,12 @@ import {
   Home,
   About
 } from 'pages';
+import Menu from 'components/Menu';
 
 const App = () => {
   return (
     <div>
+      <Menu/>
       <Route exact path ="/" component={Home} />
       <Route path="/about/:name?" component={About} />
     </div>
--- react-router-tutorial/src/pages/About.js
+++ react-router-tutorial/src/pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import queryString from 'query-string';
 
-const About = ({match}) => {
+const About = ({location, match}) => {
+    const query = queryString.parse(location.search);
+    
+    const { color } = query;
+
     return (
         <div>
-            <h2>소개</h2>
+            <h2 style={{color}}>소개</h2>
             <p>
                 안녕하세요, 저는 {match.params.name}입니다.
             </p>
