ec4e7b710c743a3f51272c1689e4d4a9c0e99753
Yeongmin Kim <oinochoe@naver.com>
2019-06-16 16:23:01 +0900

    [blog-backend] next().then() promise 사용하여 실행시키기

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,6 +2,18 @@ const Koa = require('koa');
 
 const App = new Koa();
 
+App.use((ctx, next) => {
+  console.log(1);
+  next().then(() => {
+    console.log('bye');
+  });
+});
+
+App.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
 App.use((ctx) => {
   ctx.body = 'hello world';
 });
