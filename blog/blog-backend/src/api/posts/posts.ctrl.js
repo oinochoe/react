bfe811faa964e1ac7fa589bde87fc98b68ae1d4a
oinochoe <oinochoe@naver.com>
2019-06-27 10:41:06 +0900

    export list 메서드 추가

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,4 @@
-let postId = 1; // id의 초깃값입니다.
+let postId = 1; // id의 초깃값입니다
 
 const posts = [
   {
@@ -10,8 +10,7 @@ const posts = [
 
 /* 포스트 작성
    POST /api/posts
-   { title, body }
-*/
+   { title, body } */
 exports.write = (ctx) => {
   // REST API의 request body는 ctx.request.body에서 조회할 수 있습니다.
   const {
@@ -19,7 +18,7 @@ exports.write = (ctx) => {
     body
   } = ctx.request.body;
 
-  postId += 1; // 기존 postId 값에 1을 더합니다.
+  postId += 1; // 기존의 postId 값에 1을 더합니다
 
   const post = { id: postId, title, body };
   posts.push(post);
@@ -27,17 +26,22 @@ exports.write = (ctx) => {
 };
 
 /* 포스트 목록 조회
-   GET /api/posts/:id
-*/
+   GET /api/posts */
+exports.list = (ctx) => {
+  ctx.body = posts;
+};
+
+/* 특정 포스트 조회
+   GET /api/posts/:id */
 exports.read = (ctx) => {
   const { id } = ctx.params;
 
-  // 주어진 id 값으로 포스트를 찾습니다.
-  // 파라미터로 받아 온 값은 문자열 형식이니 파라미터를 숫자로 변환하거나,
+  // 주어진 id 값으로 포스트를 찾습니다
+  // 파라미터로 받아온 값은 문자열 형식이니, 파라미터를 숫자로 변환하거나,
   // 비교할 p.id 값을 문자열로 변경해야 합니다.
   const post = posts.find(p => p.id.toString() === id);
 
-  // 포스트가 없으면 오류를 반환
+  // 포스트가 없을 경우 오류를 반환합니다.
   if (!post) {
     ctx.status = 404;
     ctx.body = {
@@ -45,19 +49,19 @@ exports.read = (ctx) => {
     };
     return;
   }
+
   ctx.body = post;
 };
 
 /* 특정 포스트 제거
-   DELETE /api/posts/:id
-*/
+   DELETE /api/posts/:id */
 exports.remove = (ctx) => {
   const { id } = ctx.params;
 
-  // 해당 id를 가진 post가 몇 번째인지 확인
-  const index = posts.findIndex(p => p.id.toString === id);
+  // 해당 id를 가진 post가 몇 번째인지 확인합니다
+  const index = posts.findIndex(p => p.id.toString() === id);
 
-  // 포스트가 없으면 오류를 반환합니다.
+  // 포스트가 없을 경우 오류를 반환합니다.
   if (index === -1) {
     ctx.status = 404;
     ctx.body = {
@@ -68,21 +72,20 @@ exports.remove = (ctx) => {
 
   // index 번째 아이템을 제거합니다.
   posts.splice(index, 1);
-  ctx.status = 204; // no Content
+  ctx.status = 204; // No Content
 };
 
-/* 포스트 수정(교체)
-   PUT /api/posts/:id
-   { title, body }
- */
+/* 포스트 수정 (교체)
+  PUT /api/posts/:id
+  { title, body } */
 exports.replace = (ctx) => {
-  // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용합니다.
+  // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용됩니다.
   const { id } = ctx.params;
 
-  // 해당 id를 가진 post가 몇 번째인지 확인합니다.
+  // 해당 id를 가진 post가 몇 번째인지 확인합니다
   const index = posts.findIndex(p => p.id.toString() === id);
 
-  // 포스트가 없으면 오류를 반환합니다.
+  // 포스트가 없을 경우 오류를 반환합니다.
   if (index === -1) {
     ctx.status = 404;
     ctx.body = {
@@ -92,7 +95,7 @@ exports.replace = (ctx) => {
   }
 
   // 전체 객체를 덮어 씌웁니다.
-  // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만듭니다.
+  // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 생성합니다.
   posts[index] = {
     id,
     ...ctx.request.body
@@ -100,18 +103,17 @@ exports.replace = (ctx) => {
   ctx.body = posts[index];
 };
 
-/* 포스트 수정(특정 필드 변경)
+/* 포스트 수정 (특정 필드 변경)
    PATCH /api/posts/:id
-   { title, body }
- */
+   { title, body } */
 exports.update = (ctx) => {
   // PATCH 메서드는 주어진 필드만 교체합니다.
   const { id } = ctx.params;
 
-  // 해당 id를 가진 posts가 몇 번째인지 확인합니다.
+  // 해당 id를 가진 post가 몇 번째인지 확인합니다
   const index = posts.findIndex(p => p.id.toString() === id);
 
-  // 포스트가 없으면 오류를 반환합니다.
+  // 포스트가 없을 경우 오류를 반환합니다.
   if (index === -1) {
     ctx.status = 404;
     ctx.body = {
@@ -126,4 +128,4 @@ exports.update = (ctx) => {
     ...ctx.request.body
   };
   ctx.body = posts[index];
-};
+}; 
