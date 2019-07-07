74e4de635b8f07d4d6c02b727a6ce7f225cc4033
Yeongmin Kim <oinochoe@naver.com>
2019-07-07 18:52:13 +0900

    write 기능과 list 불러오는 기능을 작성 후 postman에서 api 쏴서 확인 완료

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,9 +1,34 @@
 const Post = require('models/post');
 
-exports.write = (ctx) => {
+/* POST /api/posts
+ * { title, body, tags}
+ */
+exports.write = async (ctx) => {
+  const { title, body, tags } = ctx.request.body;
+
+  // 새 Post 인스턴스를 만듭니다.
+  const post = new Post({
+    title, body, tags
+  });
+
+  try {
+    await post.save(); // 데이터베이스에 등록합니다.
+    ctx.body = post; // 저장된 결과를 반환합니다.
+  } catch (e) {
+    // 데이터베이스의 오류가 발생합니다.
+    ctx.throw(e, 500);
+  }
 };
 
-exports.list = (ctx) => {
+/* GET /api/posts
+ */
+exports.list = async (ctx) => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(e, 500);
+  }
 };
 
 exports.read = (ctx) => {
