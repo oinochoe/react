f122ae46afbb1ee7bdc11ebb0fe5314e336ec490
Yeongmin <oinochoe@naver.com>
2019-07-15 10:03:44 +0900

    글로벌 스타일 설정

--- blog/blog-frontend/config/paths.js
+++ blog/blog-frontend/config/paths.js
@@ -82,7 +82,8 @@ module.exports = {
   proxySetup: resolveApp('src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json')),
+  servedPath: getServedPath(resolveApp('package.json')), 
+  globalStyles: resolveApp('src/Styles')
 };
 
 
