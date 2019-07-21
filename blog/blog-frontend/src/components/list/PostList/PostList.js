f00500ea0c7ccb8e7d656d49ee434cd120724b4f
Yeongmin Kim <oinochoe@naver.com>
2019-07-22 00:46:26 +0900

    필요한 컴포넌트들 작성 완료

---
+++ blog/blog-frontend/src/components/list/PostList/PostList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './PostList.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const PostList = () => (
+  <div>
+    PostList
+  </div>
+);
+export default PostList;
