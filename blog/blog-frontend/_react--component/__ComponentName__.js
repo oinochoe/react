a38e4474ecb78ee02b48b781a83a50abd3670502
Yeongmin Kim <oinochoe@naver.com>
2019-07-22 00:43:44 +0900

    리액트 컴포넌트 제너레이터 세팅..

---
+++ blog/blog-frontend/_react--component/__ComponentName__.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './__ComponentName__.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const __ComponentName__ = () => (
+  <div>
+    __ComponentName__
+  </div>
+);
+export default __ComponentName__;
---
+++ blog/blog-frontend/src/components/list/ListWrapper/ListWrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ListWrapper.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const ListWrapper = () => (
+  <div>
+    ListWrapper
+  </div>
+);
+export default ListWrapper;
