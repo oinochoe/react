be9642cf8b84be0664b2ee1fc9968b3e90bb0c79
Yeongmin Kim <oinochoe@naver.com>
2019-07-26 21:22:32 +0900

    generate componets 이용하여 postInfo, PostBody 생성

---
+++ blog/blog-frontend/src/components/post/PostBody/PostBody.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './PostBody.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const PostBody = () => (
+  <div>
+    PostBody
+  </div>
+);
+export default PostBody;
---
+++ blog/blog-frontend/src/components/post/PostInfo/PostInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './PostInfo.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const PostInfo = () => (
+  <div>
+    PostInfo
+  </div>
+);
+export default PostInfo;
