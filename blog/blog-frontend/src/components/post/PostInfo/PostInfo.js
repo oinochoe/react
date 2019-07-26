5e7b9144491d97980dcb2217f7a7f1a5d86f0192
Yeongmin Kim <oinochoe@naver.com>
2019-07-26 22:29:50 +0900

    Post View 페이지 진행중

--- blog/blog-frontend/src/components/post/PostInfo/PostInfo.js
+++ blog/blog-frontend/src/components/post/PostInfo/PostInfo.js
@@ -5,8 +5,17 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 const PostInfo = () => (
-  <div>
-    PostInfo
+  <div className={cx('post-info')}>
+    <div className={cx('info')}>
+      <h1>타이틀</h1>
+      <div className={cx('tags')}>
+        <a href="#none">#태그</a>
+        <a href="#none">#태그</a>
+        <a href="#none">#태그</a>
+      </div>
+      <div className={cx('date')}>Oct 29, 2017</div>
+    </div>
   </div>
 );
+
 export default PostInfo;
