f9dc518d0f79ae011b499212ca1dce8f0ef1b70f
Yeongmin Kim <oinochoe@naver.com>
2019-07-28 18:41:08 +0900

    editorTemplate 중간 separator 기능 추가 완료

--- blog/blog-frontend/src/components/editor/EditorTemplate/EditorTemplate.js
+++ blog/blog-frontend/src/components/editor/EditorTemplate/EditorTemplate.js
@@ -5,18 +5,61 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 class EditorTemplate extends Component {
+  state = {
+    leftPercentage: 0.5
+  }
+
+  // separator 클릭 후 마우스를 움직이면 그에 따라 leftPercentage 업데이트
+  handleMouseMove = (e) => {
+    this.setState({
+      leftPercentage: e.clientX / window.innerWidth
+    });
+  }
+
+  // 마우스를 뗏을 때 등록한 이벤트 제거
+  handleMouseUp = (e) => {
+    document.body.removeEventListener('mousemove', this.handleMouseMove);
+    window.removeEventListener('mouseup', this.handleMouseUp);
+  }
+
+  // seperator
+  handleSeperatorMouseDown = (e) => {
+    document.body.addEventListener('mousemove', this.handleMouseMove);
+    window.addEventListener('mouseup', this.handleMouseUp);
+  }
+
   render() {
     const { header, editor, preview } = this.props;
+    const { leftPercentage } = this.state;
+    const { handleSeperatorMouseDown } = this;
+
+    // 각 영역에 flex 값 적용
+    const leftStyle = {
+      flex: leftPercentage
+    };
+    const rightStyle = {
+      flex: 1 - leftPercentage
+    };
+
+    // seperator 위치 설정
+    const seperatorStyle = {
+      left: `${leftPercentage * 100}%`
+    };
+
     return (
       <header className={cx('editor-template')}>
         {header}
         <div className={cx('panes')}>
-          <div className={cx('pane', 'editor')}>
+          <div className={cx('pane', 'editor')} style={leftStyle}>
             {editor}
           </div>
-          <div className={cx('pane', 'preview')}>
+          <div className={cx('pane', 'preview')} style={rightStyle}>
             {preview}
           </div>
+          <div
+            className={cx('seperator')}
+            style={seperatorStyle}
+            onMouseDown={handleSeperatorMouseDown}/>
         </div>
       </header>
     );
