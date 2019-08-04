42d029c6cfe600a797138802da2847c9bb7d5445
Yeongmin Kim <oinochoe@naver.com>
2019-08-04 15:38:19 +0900

    editor 모듈에 WRITE_POST 액션 생성

--- blog/blog-frontend/src/store/modules/editor.js
+++ blog/blog-frontend/src/store/modules/editor.js
@@ -2,20 +2,24 @@ import { createAction, handleActions } from 'redux-actions';
 
 import { Map } from 'immutable';
 import { pender } from 'redux-pender';
+import * as api from 'lib/api';
 
 // action types
 const INITIALIZE = 'editor/INITIALIZE';
 const CHANGE_INPUT = 'editor/CHANGE_INPUT';
+const WRITE_POST = 'editor/WRITE_POST';
 
 // action creators
 export const initialize = createAction(INITIALIZE);
 export const changeInput = createAction(CHANGE_INPUT);
+export const writePost = createAction(WRITE_POST, api.writePost);
 
 // initial state
 const initialState = Map({
   title: '',
   markdown: '',
-  tags: ''
+  tags: '',
+  postId: null
 });
 
 // reducer
@@ -24,6 +28,12 @@ export default handleActions({
   [CHANGE_INPUT]: (state, action) => {
     const { name, value } = action.payload;
     return state.set(name, value);
-  }
-
+  },
+  ...pender({
+    type: WRITE_POST,
+    onSuccess: (state, action) => {
+      const { _id } = action.payload.data;
+      return state.set('postId', _id);
+    }
+  })
 }, initialState)
