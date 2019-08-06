370b2f1c25c5d08f520d35050106634dc66b09d2
Yeongmin <oinochoe@naver.com>
2019-08-06 10:00:43 +0900

    getPostList 가져와서 ListPage에서 전달받을 컴포넌트 생성 진행중

---
+++ blog/blog-frontend/src/containers/list/ListContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PostList from 'components/list/PostList';
+import Pagination from 'components/list/Pagination';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as listActions from 'store/modules/list';
+
+class ListContainer extends Component {
+  
+}
