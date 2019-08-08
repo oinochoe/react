d97def20ba04a999fe3c387a2d8cb521ccdef5e3
Yeongmin <oinochoe@naver.com>
2019-08-08 10:12:05 +0900

    remove-markdown 으로 태그 이슈 정리 및 PostList  데이터로 렌더링 리스트 페이지 기본 설정

--- blog/blog-frontend/src/pages/ListPage.js
+++ blog/blog-frontend/src/pages/ListPage.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import ListWrapper from 'components/list/ListWrapper';
-import PostList from 'components/list/PostList';
-import Pagination from 'components/list/Pagination';
+import ListContainer from 'components/list/ListContainer';
 
-const ListPage = () => {
+const ListPage = ({match}) => {
+  // page의 기본 값을 1로 설정
+  const { page = 1, tag } = match.params;
+
   return (
     <PageTemplate>
       <ListWrapper>
-        <PostList/>
-        <Pagination/>
+        <ListContainer
+          page={parseInt(page, 10)}
+          tag={tag}
+        />
       </ListWrapper>
     </PageTemplate>
   );
