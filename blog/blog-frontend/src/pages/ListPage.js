ad7d91568a9bb5cb854c96b28050b12de4461905
Yeongmin <oinochoe@naver.com>
2019-08-09 10:09:37 +0900

    import 잘못되었던 부분 수정 완료.. 아직 List가 나오지 않는다 ㅜㅜ

--- blog/blog-frontend/src/pages/ListPage.js
+++ blog/blog-frontend/src/pages/ListPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import ListWrapper from 'components/list/ListWrapper';
-import ListContainer from 'components/list/ListContainer';
+import ListContainer from 'containers/list/ListContainer';
 
 const ListPage = ({match}) => {
   // page의 기본 값을 1로 설정
