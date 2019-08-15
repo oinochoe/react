b6a1a6424988c61a45020ba703770cfb5bcb2b70
Yeongmin Kim <oinochoe@naver.com>
2019-08-15 13:39:50 +0900

    EditorHeaderContainer 경로 수정

--- blog/blog-frontend/src/pages/EditorPage.js
+++ blog/blog-frontend/src/pages/EditorPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import EditorTemplate from 'components/editor/EditorTemplate';
-import EditorHeaderContainer from 'components/editor/EditorHeaderContainer';
+import EditorHeaderContainer from 'containers/editor/EditorHeaderContainer';
 import EditorPaneContainer from 'containers/editor/EditorPaneContainer';
 import PreviewPaneContainer from 'containers/editor/PreviewPaneContainer';
 
--- blog/blog-frontend/src/store/modules/editor.js
+++ blog/blog-frontend/src/store/modules/editor.js
@@ -8,11 +8,13 @@ import * as api from 'lib/api';
 const INITIALIZE = 'editor/INITIALIZE';
 const CHANGE_INPUT = 'editor/CHANGE_INPUT';
 const WRITE_POST = 'editor/WRITE_POST';
+const GET_POST = 'editor/GET_POST';
 
 // action creators
 export const initialize = createAction(INITIALIZE);
 export const changeInput = createAction(CHANGE_INPUT);
 export const writePost = createAction(WRITE_POST, api.writePost);
+export const getPost = createAction(GET_POST, api.getPost);
 
 // initial state
 const initialState = Map({
@@ -34,6 +36,15 @@ export default handleActions({
     onSuccess: (state, action) => {
       const { _id } = action.payload.data;
       return state.set('postId', _id);
+    }
+  }),
+  ...pender({
+    type: GET_POST,
+    onSuccess: (state, action) => {
+      const { title, tags, body } = action.payload.data;
+      return state.set('title', title)
+                  .set('markdown', body)
+                  .set('tags', tags.join(', ')); // 배열 -> , 로 구분된 문자열
     }
   })
 }, initialState)
