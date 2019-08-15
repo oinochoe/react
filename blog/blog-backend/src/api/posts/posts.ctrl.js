3e3e41b0087050deeeb6ec165093155766428fff
Yeongmin Kim <oinochoe@naver.com>
2019-08-15 12:58:16 +0900

    tag 맵핑 백엔드 기능 추가 및 Header container 컴포넌트 만들어둠

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -58,6 +58,11 @@ exports.list = async (ctx) => {
   // page가 주어지지 않았다면 1로 간주
   // query는 문자열 형태로 받아 오므로 숫자로 변환
   const page = parseInt(ctx.query.page || 1, 10);
+  const { tag } = ctx.query;
+
+  const query = tag ? {
+    tags : tag // tags 배열에 tag를 가진 포스트 찾기
+  } : {};
 
   // 잘못된 페이지가 주어졌다면 오류
   if (page < 1) {
@@ -66,17 +71,17 @@ exports.list = async (ctx) => {
   }
 
   try {
-    const posts = await Post.find()
+    const posts = await Post.find(query)
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
       .lean()
       .exec();
 
-    const postCount = await Post.countDocuments().exec();
+    const postCount = await Post.countDocuments(query).exec();
     const limitBodyLength = post => ({
       ...post,
-      body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+      body: post.body.length < 350 ? post.body : `${post.body.slice(0, 350)}...`
     });
     ctx.body = posts.map(limitBodyLength);
     // 마지막 페이지 알려 주기
