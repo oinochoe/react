ce9a03faa08c867457dd3b9feca58ab9c693201c
Yeongmin <oinochoe@naver.com>
2019-08-16 10:03:17 +0900

    모달 animation 구현 완료

--- blog/blog-frontend/src/components/modal/ModalWrapper/ModalWrapper.js
+++ blog/blog-frontend/src/components/modal/ModalWrapper/ModalWrapper.js
@@ -5,14 +5,45 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 class ModalWrapper extends Component {
+  state = {
+    animate: false
+  }
+
+  startAnimation = () => {
+    // animate 값을 true로 설정 후
+    this.setState({
+      animate: true
+    });
+    // 250ms 이후 다시 false로 설정
+    setTimeout(() => {
+      this.setState({
+        animate: false
+      })
+    }, 250);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevProps.visible !== this.props.visible) {
+      this.startAnimation();
+    }
+  }
+
   render() {
     const { children, visible } = this.props;
-    if(!visible) return null;
+    const { animate } = this.state;
+
+    // visible과 animate 값이 둘다 false일 때만
+    // null을 리턴
+    if(!visible && !animate) return null;
+
+    // 상태에 따라 애니메이션 설정
+    const animation = animate && (visible ? 'enter' : 'leave');
+    
     return (
       <div>
-        <div className={cx('gray-background')} />
+        <div className={cx('gray-background', animation)} />
         <div className={cx('modal-wrapper')}>
-          <div className={cx('modal')}>
+          <div className={cx('modal', animation)}>
             {children}
           </div>
         </div>
