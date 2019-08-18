9b11ddaca92858aed16d6f008bbc682a1155ea4a
Yeongmin Kim <oinochoe@naver.com>
2019-08-18 16:05:54 +0900

    로그인 모달 구현 완료

--- blog/blog-frontend/src/containers/modal/LoginModalContainer.js
+++ blog/blog-frontend/src/containers/modal/LoginModalContainer.js
@@ -5,30 +5,42 @@ import { bindActionCreators } from 'redux';
 import * as baseActions from 'store/modules/base';
 
 class LoginModalContainer extends Component {
-  handleLogin = () => {
-
+  handleLogin = async () => {
+    const { BaseActions, password } = this.props;
+    try {
+      // 로그인 시도, 성공하면 모달 닫기
+      await BaseActions.login(password);
+      BaseActions.hideModal('login');
+    } catch(e) {
+      console.log(e);
+    }
   }
   handleCancel = () => {
     const { BaseActions } = this.props;
     BaseActions.hideModal('login');
   }
   handleChange = (e) => {
-
+    const { value } = e.target;
+    const { BaseActions } = this.props;
+    BaseActions.changePasswordInput(value);
   }
   handleKeyPress = (e) => {
-
+    // 엔터 키를 누르면 로그인 호출
+    if(e.key === 'Enter') {
+      this.handleLogin();
+    }
   }
   render() {
     const {
       handleLogin, handleCancel, handleChange, handleKeyPress
     } = this;
-    const { visible } = this.props;
+    const { visible, error, password } = this.props;
 
     return (
       <LoginModal
         onLogin={handleLogin} onCancel={handleCancel}
         onChange={handleChange} onKeyPress={handleKeyPress}
-        visible={visible}
+        visible={visible} error={error} password={password}
       />
     );
   }
@@ -36,7 +48,9 @@ class LoginModalContainer extends Component {
 
 export default connect(
   (state) => ({
-    visible: state.base.getIn(['modal', 'login'])
+    visible: state.base.getIn(['modal', 'login']),
+    password: state.base.getIn(['loginModal', 'password']),
+    error: state.base.getIn(['loginModal', 'error'])
   }),
   (dispatch) => ({
     BaseActions: bindActionCreators(baseActions, dispatch)
--- blog/blog-frontend/src/store/modules/base.js
+++ blog/blog-frontend/src/store/modules/base.js
@@ -2,22 +2,40 @@ import { createAction, handleActions } from 'redux-actions';
 
 import { Map } from 'immutable';
 import { pender } from 'redux-pender';
+import * as api from 'lib/api';
 
 // action types
 const SHOW_MODAL = 'base/SHOW_MODAL';
 const HIDE_MODAL = 'base/HIDE_MODAL';
+const LOGIN = 'base/LOGIN';
+const LOGOUT = 'base/LOGOUT';
+const CHECK_LOGIN = 'base/CHECK_LOGIN';
+const CHANGE_PASSWORD_INPUT = 'base/CHANGE_PASSWORD_INPUT';
+const INITIALIZE_LOGIN_MODAL = 'base/INITIALIZE_LOGIN_MODAL';
 
 // action creators
 export const showModal = createAction(SHOW_MODAL);
 export const hideModal = createAction(HIDE_MODAL);
+export const login = createAction(LOGIN, api.login);
+export const logout = createAction(LOGOUT, api.logout);
+export const checkLogin = createAction(CHECK_LOGIN, api.checkLogin);
+export const changePasswordInput = createAction(CHANGE_PASSWORD_INPUT);
+export const initializeLoginModal = createAction(INITIALIZE_LOGIN_MODAL);
+
 
 // initial state
 const initialState = Map({
   // 모달의 가시성 상태
-  modal: Map({ 
+  modal: Map({
     remove: false,
     login: false // 추후 구현될 로그인 모달
-  })
+  }),
+  // 로그인 모달 상태
+  loginModal: Map({
+    password: '',
+    error: false
+  }),
+  logged: false // 현재 로그인상태 초기화
 });
 
 // reducer
@@ -29,5 +47,30 @@ export default handleActions({
   [HIDE_MODAL]: (state, action) => {
     const { payload: modalName } = action;
     return state.setIn(['modal', modalName], false);
-  }
+  },
+  ...pender({
+    type: LOGIN,
+    onSuccess: (state, action) => { // 로그인에 성공할 때
+      return state.set('logged', true);
+    },
+    onError: (state, action) => { // 오류가 발생할 때
+      return state.setIn(['loginModal', 'error'], true)
+                  .setIn(['loginModal', 'password'], '');
+    }
+  }),
+  ...pender({
+    type: CHECK_LOGIN,
+    onSuccess: (state, action) => {
+      const { logged } = action.payload.data;
+      return state.set('logged', logged);
+    }
+  }),
+  [CHANGE_PASSWORD_INPUT]: (state, action) => {
+    const { payload: value } = action;
+    return state.setIn(['loginModal', 'password'], value);
+  },
+  [INITIALIZE_LOGIN_MODAL]: (state, action) => {
+    // 로그인 모달의 상태를 초기 상태로 설정(텍스트/오류 초기화)
+    return state.set('loginModal', initialState.get('loginModal'));
+  },
 }, initialState)
